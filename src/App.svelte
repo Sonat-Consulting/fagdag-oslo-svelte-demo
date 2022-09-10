c63d3760bfa199e6253d455d35bc26f87b2c48c7
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-09-10 11:12:47 +0200

    chore: setup daisy ui tailwind plugin

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,8 @@
 
 </script>
 
-<main class="container mx-auto px-5">
-    <h1 class="text-center text-4xl">Hello world!!</h1>
+<main class="container mx-auto p-5">
+    <h1 class="text-center text-4xl">Hello world</h1>
 </main>
 
 <style>
