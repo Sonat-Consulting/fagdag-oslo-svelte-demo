692ea57aa8adde4fa54c789a8888b4f50d9bd76c
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-09-14 19:03:42 +0200

    add steps html to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,32 @@
 <script lang="ts">
-    let work = {
-        title: "Present svelte",
-        description: "Explain what Svelte is"
-    }
+  let work = {
+    title: "Present svelte",
+    description: "Explain what Svelte is"
+  }
 </script>
 
 <main class="container mx-auto p-5">
-    <h1 class="text-center text-4xl">Hello world</h1>
+  <h1 class="text-center text-4xl">Hello world</h1>
 
-    <div class="card w-96 bg-base-100 shadow-xl mx-auto">
-        <div class="card-body">
-            <h2 class="card-title">{work.title}</h2>
-            <p class="mb-3">{work.description}</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Done</button>
-            </div>
-        </div>
+  <div class="flex justify-center mb-10">
+    <ul class="steps steps-vertical lg:steps-horizontal">
+      <li class="step">Step 1</li>
+      <li class="step">Step 2</li>
+      <li class="step">Step 3</li>
+      <li class="step">Step 4</li>
+    </ul>
+  </div>
+
+
+  <div class="card w-96 bg-base-100 shadow-xl mx-auto">
+    <div class="card-body">
+      <h2 class="card-title">{work.title}</h2>
+      <p class="mb-3">{work.description}</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Done</button>
+      </div>
     </div>
+  </div>
 </main>
 
 <style>
