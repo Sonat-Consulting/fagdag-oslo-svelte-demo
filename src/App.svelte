a6ff3dc01a4168359ba804be5833c64f0c7d3073
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-09-14 21:55:19 +0200

    add statistics component with data from same store

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import Work from "./components/Work.svelte"
+  import Statistics from "./components/Statistics.svelte"
 </script>
 
 <main class="container mx-auto p-5">
   <h1 class="text-center text-4xl mb-10">Work to do</h1>
   <Work />
+  <Statistics />
 </main>
 
 <style>
