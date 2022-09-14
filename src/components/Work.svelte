f8de4648db461ff6abf200a29312f6af157c44f0
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-09-14 21:38:52 +0200

    demo svelte store with auto subscription

--- src/components/Work.svelte
+++ src/components/Work.svelte
@@ -2,32 +2,22 @@
   import Steps from "./Steps.svelte"
   import Card from "./Card.svelte"
   import Success from "./Success.svelte"
+  import { steps } from "../stores/work.store"
 
-  import { steps as store } from "../stores/work.store"
-  import { onDestroy } from "svelte"
-
-  let steps: Work[] = []
-
-  const unsubscribe = store.subscribe(stepsInStore => steps = stepsInStore)
-
-  onDestroy(() => {
-    unsubscribe()
-  })
-
-  $: activeStep = steps.find(step => !step.completed)
+  $: activeStep = $steps.find(step => !step.completed)
 
   $: {
     console.log(`active step is now ${activeStep?.id}`)
   }
 
   const completeTask = () => {
-    store.update(currentStepsInStore =>
+    steps.update(currentStepsInStore =>
       currentStepsInStore.map(step => step.id === activeStep.id ? { ...step, completed: true } : step)
     )
   }
 </script>
 
-<Steps {steps} />
+<Steps steps={$steps} />
 
 {#if activeStep}
   <Card {activeStep} {completeTask} />
