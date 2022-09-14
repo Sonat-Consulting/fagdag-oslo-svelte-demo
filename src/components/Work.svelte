aace566b95f09e5d68d6811e68083491e4913a77
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-09-14 20:56:47 +0200

    demo svelte store with manual subscription

--- src/components/Work.svelte
+++ src/components/Work.svelte
@@ -3,38 +3,16 @@
   import Card from "./Card.svelte"
   import Success from "./Success.svelte"
 
-  let steps: Work[] = [
-    {
-      id: "1",
-      title: "Getting started",
-      completed: true,
-      description: "Set up project and install dependencies"
-    },
-    {
-      id: "2",
-      title: "Creating sub components",
-      completed: false,
-      description: "Create sub component to show how Svelte code modularization works"
-    },
-    {
-      id: "3",
-      title: "Passing props",
-      completed: false,
-      description: "Demonstrate how data is passed between components in Svelte"
-    },
-    {
-      id: "4",
-      title: "Triggering events",
-      completed: false,
-      description: "Demonstrate how sub components can notify parent components of data change"
-    },
-    {
-      id: "5",
-      title: "Finalize",
-      completed: false,
-      description: "Do this one last thing"
-    }
-  ]
+  import { steps as store } from "../stores/work.store"
+  import { onDestroy } from "svelte"
+
+  let steps: Work[] = []
+
+  const unsubscribe = store.subscribe(stepsInStore => steps = stepsInStore)
+
+  onDestroy(() => {
+    unsubscribe()
+  })
 
   $: activeStep = steps.find(step => !step.completed)
 
